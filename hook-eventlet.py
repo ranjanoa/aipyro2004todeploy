dfa32a3e078ab1935fcee4b827c8dd401f04dba8
ranjanoa <ranjanoa@gmail.com>
2026-04-22 12:36:19 +0200

    Fix: Make eventlet runtime hook more aggressive and add dnspython patching

--- hook-eventlet.py
+++ hook-eventlet.py
@@ -1,3 +1,10 @@
 import eventlet
-eventlet.monkey_patch()
-print("[INIT] Eventlet Monkey Patch applied via Runtime Hook")
+eventlet.monkey_patch(all=True)
+import dns
+# Force dns to use eventlet's socket if it was already loaded
+try:
+    import eventlet.support.dnspython
+    eventlet.support.dnspython.patch_dnspython()
+except:
+    pass
+print("[INIT] AGGRESSIVE Eventlet Monkey Patch applied")
